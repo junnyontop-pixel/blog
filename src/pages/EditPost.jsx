c73727c01e5aeb328413b6282cefa905f1b741c5
ayj8201 <junny.on.top@gmail.com>
2025-12-27 06:22:24 +0000

    Pending changes exported from your codespace

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,10 @@
+function EditPost() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Edit Page</h1>
+      <p>편집 페이지 준비 중</p>
+    </div>
+  );
+}
+
+export default EditPost;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,65 @@
+import '../App.css';
+import { useState } from "react";
+
+function Home() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "파이썬 독학",
+      content: "파이썬 어쩌구저쩌구....",
+    },
+    {
+      id: 2,
+      title: "js 독학",
+      content: "js 어쩌구저쩌구....",
+    },
+    {
+      id: 3,
+      title: "자바 독학",
+      content: "자바 어쩌구저쩌구....",
+    },
+  ]);
+
+  const [selectedPostId, setSelectedPostId] = useState(null);
+
+  return (
+    <>
+      <div className="header_container">
+        <header id='header'>
+          <div id="bar_container">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+          </div>
+          <div id="title_container">
+            <h2 id='title'>Blog</h2>
+          </div>
+          <div id="login_container">
+            <button id="login_btn">login</button>
+          </div>
+          <div id="signup_container">
+            <button id="signup_btn">sign up</button>
+          </div>
+          {/* 나중에 검색기능 추가 */}
+
+          {/* 여기에 */}
+
+        </header>
+      </div>
+      <div id="content_container">
+        {posts.map((post) => (
+          <div className="contents" key={post.id}>
+            <h4 className="title">{post.title}</h4>
+            <p className="main_content">{post.content}</p>
+
+            {/* 좋아요 아이콘 */}
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg>
+
+            {/* 최고에요 아이콘 */}
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="m713-280 127-297v-23H588l19-134-55 55q-8-8-28-28.5T496-736l144-144 32 32q14 14 19.5 31.5T694-780l-14 100h160q33 0 56.5 23.5T920-600v23q0 8-1.5 16t-4.5 16l-93 216q-10 22-30 35.5T747-280h-34ZM240-120H40v-400h200v80H120v240h120v80Zm267 0H160v-360l240-240 32 32q14 14 19.5 31.5T454-620l-14 100h160q33 0 56.5 23.5T680-440v24q0 8-1.5 15.5T674-385l-93 217q-10 22-30 35t-44 13Zm0-80 93-216v-24H348l19-134-127 127v247h267Zm-267 0v-247 247Z"/></svg>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Home;
